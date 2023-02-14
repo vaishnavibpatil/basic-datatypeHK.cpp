b90b5364d27bb3971f0a738b7018b27f3033cdb8
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2023-02-14 22:47:45 +0530

    Add files via upload

---
+++ basic data typeHK.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include<cstdio>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    long y;
+    char z;
+    float w;
+    double t;
+
+    scanf("%d %ld %c %f %lf",&x,&y,&z,&w,&t);
+    printf("%d\n%ld\n%c\n%f\n%lf",x,y,z,w,t);
+    return 0;
+}
