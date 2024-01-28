d1ba3caeff4f666fdb205f213f83f299be73dabd
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-01-28 22:09:23 +0530

    Add files via upload

---
+++ addition.cpp.java
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int ch;
+   cin>>ch;
+   if(ch==1)
+   {
+       cout<<"one";
+   }
+   else if(ch==2)
+   {
+       cout<<"two";
+   }
+    else if(ch==3)
+   {
+       cout<<"three";
+   }
+    else if(ch==4)
+   {
+       cout<<"four";
+   }
+    else if(ch==5)
+   {
+       cout<<"five";
+   }
+    else if(ch==6)
+   {
+       cout<<"six";
+   }
+    else if(ch==7)
+   {
+       cout<<"seven";
+   }
+    else if(ch==8)
+   {
+       cout<<"eight";
+   }
+    else if(ch==9)
+   {
+       cout<<"nine";
+   }
+   else 
+   {
+       cout<<"Greater than 9";
+   }
+}
+   
+   
+    
+
+s
